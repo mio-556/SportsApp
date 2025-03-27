21db4df659dd3b17819f71dc31af085ba5fa237f
mio-556 <svancara.michall@gmail.com>
2025-03-27 12:15:31 +0100

    creating flexible activity store using generics

---
+++ vue/src/stores/activityStore.ts
@@ -0,0 +1,14 @@
+import { defineStore } from 'pinia'
+import { computed, ref } from 'vue'
+import type { BaseActivity } from '@/types/activityTypes'
+
+export const createActivityStore = <T extends BaseActivity>(storeId: string) => {
+  return defineStore(storeId, () => {
+    const activities = ref<T[]>([])
+    const totalDistance = computed(() => {
+      return activities.value.reduce((total, item) => total + (item.distance ?? 0), 0)
+    })
+
+    return { activities, totalDistance }
+  })
+}
